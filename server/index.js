0ee1bbd094c170689fd8f40695e161bbdadfeb72
dropella-dev <austinandogola@gmail.com>
2025-02-28 09:47:07 +0300

    set to aus

--- server/index.js
+++ server/index.js
@@ -136,8 +136,8 @@ function getFormattedDates() {
 }
 
 // let fromEmailString=['FROM', 'bunkers@clipperoil.com']
-let fromEmailString=['FROM', 'kevin@clipperoil.com']
-// let fromEmailString=['FROM', 'austinandogola@gmail.com']
+// let fromEmailString=['FROM', 'kevin@clipperoil.com']
+let fromEmailString=['FROM', 'austinandogola@gmail.com']
 let subJectString='SEA Card® OMSQuote Window Opened'
 // setInterval(() => {
 //   checkEmails();
